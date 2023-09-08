3707308db16505dad0efc08a7204a975483c283a
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-09-08 08:37:38 +0100

    update: modified time

--- script.js
+++ script.js
@@ -5,11 +5,7 @@ day.textContent =`${weekDays[currentDate.getDay()]} `
 const time = document.getElementById("time")
 function updateTime(){
     const currentTime = new Date();
-    const hours = currentTime.getHours();
-    const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-    const seconds = currentTime.getSeconds().toString().padStart(2, '0');
-    const amPm = hours >= 12 ? 'PM' : 'AM' 
-    const timeString = `${hours.toString().padStart(2, '0')}:${minutes}:${seconds} ${amPm}`;
+    const timeString = currentTime.getTime();
     time.textContent =timeString 
 
 }
