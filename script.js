3f0259a8017f43c627b7087370d85fbc2aa63319
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-09-08 08:18:02 +0100

    update: removed timestamp, modified userName attribute

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 const currentDate = new Date()
 const day = document.getElementById("day")
-day.textContent =`${weekDays[currentDate.getDay()]}, `
+day.textContent =`${weekDays[currentDate.getDay()]} `
 const time = document.getElementById("time")
 function updateTime(){
     const currentTime = new Date();
@@ -9,8 +9,7 @@ function updateTime(){
     const minutes = currentTime.getMinutes().toString().padStart(2, '0');
     const seconds = currentTime.getSeconds().toString().padStart(2, '0');
     const amPm = hours >= 12 ? 'PM' : 'AM' 
-    const utcTime = currentTime.getTime().toString()
-    const timeString = `${hours.toString().padStart(2, '0')}:${minutes}:${seconds} ${amPm} | ${utcTime} `;
+    const timeString = `${hours.toString().padStart(2, '0')}:${minutes}:${seconds} ${amPm}`;
     time.textContent =timeString 
 
 }
