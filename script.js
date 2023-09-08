8d124598c664ebf7aabe40dcc271e15572da0765
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-09-08 07:14:52 +0100

    update: changed time render format

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ const day = document.getElementById("day")
 day.textContent =`${weekDays[currentDate.getDay()]}, `
 const time = document.getElementById("time")
 function updateTime(){
-    const date = new Date()
- time.textContent =date.toISOString()
-}
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+    const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+    const amPm = hours >= 12 ? 'PM' : 'AM' 
+    const utcTime = currentTime.getTime().toString()
+    const timeString = `${hours.toString().padStart(2, '0')}:${minutes}:${seconds} ${amPm} | ${utcTime} `;
+    time.textContent =timeString 
 
-setInterval(updateTime(), 1000)
+}
+updateTime()
+setInterval(updateTime, 1000)
